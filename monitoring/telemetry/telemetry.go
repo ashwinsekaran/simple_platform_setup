d12771b39b66a72aa2dfb64b3d5b1616987eb669
ashwin sekaran <ashwin.sekaran@tc1.io>
2026-03-15 15:19:55 +0100

    Fix otel sdk version for semcov lib issue

--- monitoring/telemetry/telemetry.go
+++ monitoring/telemetry/telemetry.go
@@ -11,7 +11,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 )
 
 func Init(ctx context.Context, serviceName string) (func(context.Context) error, error) {
